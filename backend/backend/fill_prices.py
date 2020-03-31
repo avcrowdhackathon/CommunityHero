85d022221f231b8d55535396bce8f1036869c187
Christos Falas <raspberrymaker@gmail.com>
2020-04-01 01:28:24 +0300

    added script to generate prices

---
+++ backend/backend/fill_prices.py
@@ -0,0 +1,15 @@
+from backend.models import *
+import numpy as np
+import random
+
+probability = 0.5
+mean_price = 4
+stdev = 1
+shops = Shop.objects.filter(ShopTypeID='2')
+products = Product.objects.all()
+for shop in shops:
+    for product in products:
+        if product.ProductTypeID in [8,9,10,37]: continue
+        if random.random() < probability:
+            price = Price(ShopID=shop, ProductID=product, Price=np.random.normal(mean_price, stdev))
+            price.save()
