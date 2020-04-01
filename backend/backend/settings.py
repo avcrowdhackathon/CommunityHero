e7bcb5266702702cfd23e986a7078bdee40367da
Christos Falas <raspberrymaker@gmail.com>
2020-04-01 23:15:23 +0300

    Changed backend to postgresql

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'panwise',
+        'USER': os.environ.get('PGUSER'),
+        'PASSWORD': os.environ.get('PGPASS'),
+        'HOST': 'localhost'
     }
 }
 
