ff1da6c0f0c36aa4afc2b7dddd216cb034ab2ffd
Christos Falas <raspberrymaker@gmail.com>
2020-04-02 08:11:54 +0300

    Added CORS support

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -39,10 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'backend',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
