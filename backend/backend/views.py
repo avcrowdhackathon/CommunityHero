a81465f736d62ae6330ee61551cd06019d37aebc
Christos Falas <raspberrymaker@gmail.com>
2020-04-03 16:37:14 +0300

    Changed /api/create

--- backend/backend/views.py
+++ backend/backend/views.py
@@ -149,9 +149,7 @@ def create_data(request):
 				continue
 			shop = random.choice(Shop.objects.all())
 			available_items = Price.objects.all().filter(ShopID=shop.ShopID)
-			num_of_items = 0
-			while num_of_items<1 or num_of_items>len(available_items):
-				num_of_items = int(np.random.normal(7, 2))
+			num_of_items = random.randint(1, len(available_items)-1)
 
 			items = random.sample(list(available_items), num_of_items)
 			
