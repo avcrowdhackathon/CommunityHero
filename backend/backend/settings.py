085abb5351a7d1bdc865a7421f8ad897cd738402
Christos Falas <raspberrymaker@gmail.com>
2020-04-03 16:17:39 +0300

    Changed DB connection

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import django_heroku
-
+import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -79,13 +79,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'panwise',
-        'USER': os.environ.get('PGUSER'),
-        'PASSWORD': os.environ.get('PGPASS'),
-        'HOST': 'localhost'
-    }
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
 
 
