7bc7d1b81cf3c38f8f70d6c370ae0c4b86b7c86c
Christos Falas <raspberrymaker@gmail.com>
2020-04-03 16:42:02 +0300

    add prices for pharmacies

--- backend/backend/views.py
+++ backend/backend/views.py
@@ -134,6 +134,20 @@ def order_by_id(request):
 @api_view(['GET'])
 def create_data(request):
 	if request.method == 'GET':
+		probability = 0.8
+		mean_price = 4
+		stdev = 1
+		shops = Shop.objects.filter(ShopTypeID=1)
+		products = Product.objects.all()
+		for shop in shops:
+			for product in products:
+				if product.ProductTypeID.ProductTypeID in [8,9,10,37]:
+					print(shop, product)
+					if random.random() < probability:
+						print('saving')
+						price = Price(ShopID=shop, ProductID=product, Price=np.random.normal(mean_price, stdev))
+						price.save()
+
 		num_users = 100
 
 		for j in range(num_users):
